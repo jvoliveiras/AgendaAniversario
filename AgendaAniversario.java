a13f38dd732384b4bb304bc57f40c3feebdbe15b
nicaoLR004 <105064216+nicaoLR004@users.noreply.github.com>
2025-03-31 23:16:34 -0300

    Update AgendaAniversario.java

--- AgendaAniversario.java
+++ AgendaAniversario.java
@@ -11,7 +11,7 @@ public class AgendaAniversario {
         AgendaAniversario agenda = new AgendaAniversario();
         char opcao = 0;
         
-        System.out.println("PROGRAMA DESENVOLVIDO POR JOÃO VICTOR OLIVEIRA E NICOLAS.");
+        System.out.println("PROGRAMA DESENVOLVIDO POR JOÃO VICTOR OLIVEIRA E NICOLAS LIMA.");
         do {
             System.out.println("\n MENU - Agenda de Aniversário:"+
             "\n1 - Adicionar nova pessoa."+
@@ -28,13 +28,16 @@ public class AgendaAniversario {
                     agenda.menuAdicionarPessoa();
                     break;
                 case '2':
-
+                    agenda.menuRemoverPessoa();
                 case '3':
-                    break;
+                    agenda.menuPesquisarPorNome();    
+                break;
                 case '4':
-                    break;
+                    agenda.menuPesquisarDiaMes();    
+                break;
                 case '5':
-                    break;
+                    agenda.menuPesquisarMes();    
+                break;
                 case '6':
 
                     break;
@@ -66,7 +69,7 @@ public class AgendaAniversario {
             email = scan.next();
 
             while (true) {
-                System.out.println("Informe a data de aniversário (Ex: 10/2): ");
+                System.out.println("Informe a data de aniversário (Ex: 20/5): ");
                 String dataInput = scan.next();
 
                 if(Data.validaData(dataInput)){
@@ -84,7 +87,63 @@ public class AgendaAniversario {
             System.out.println("\nErro:" + e);
         }
     }
+    void menuRemoverPessoa() {
+        System.out.println("Informe o nome da pessoa a remover: ");
+        String nome = scan.next();
 
+        for (Pessoa p : arrayPessoas) {
+            if (p.getNome().equalsIgnoreCase(nome)) {
+                arrayPessoas.remove(p);
+                System.out.println("Pessoa removida com sucesso!");
+                return;
+            }
+        }
+        System.out.println("NOME INEXISTENTE");
+    }
+
+    void menuPesquisarPorNome() {
+        System.out.println("Informe o nome da pessoa: ");
+        String nome = scan.next();
+
+        for (Pessoa p : arrayPessoas) {
+            if (p.getNome().equalsIgnoreCase(nome)) {
+                System.out.println(p);
+                return;
+            }
+        }
+        System.out.println("NOME INEXISTENTE");
+    }
+
+    void menuPesquisarDiaMes() {
+        System.out.println("Informe o dia e mês do aniversário (Ex: 20/5): ");
+        String dataInput = scan.next();
+        String[] partes = dataInput.split("/");
+        int dia = Integer.parseInt(partes[0]);
+        int mes = Integer.parseInt(partes[1]);
+
+        boolean encontrado = false;
+        for (Pessoa p : arrayPessoas) {
+            if (p.getDataDeAniversario().getDia() == dia && p.getDataDeAniversario().getMes() == mes) {
+                System.out.println(p.getNome());
+                encontrado = true;
+            }
+        }
+        if (!encontrado) System.out.println("NENHUM ANIVERSARIANTE");
+    }
+
+    void menuPesquisarMes() {
+        System.out.println("Informe o mês do aniversário: ");
+        int mes = scan.nextInt();
+
+        boolean encontrado = false;
+        for (Pessoa p : arrayPessoas) {
+            if (p.getDataDeAniversario().getMes() == mes) {
+                System.out.println(p.getNome() + " - " + p.getDataDeAniversario());
+                encontrado = true;
+            }
+        }
+        if (!encontrado) System.out.println("NENHUM ANIVERSARIANTE");
+    }
     boolean validaNome(String nome) {
         for (int i = 0; i < arrayPessoas.size(); i++) {
             Pessoa tempPessoa = arrayPessoas.get(i);
